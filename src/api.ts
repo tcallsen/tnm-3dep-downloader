c4fb890fe5e51df76b4a608427180bb4d82b311e
Taylor Callsen <gritto2008@gmail.com>
2023-05-27 12:49:45 -0700

    fixing url param issue

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,20 @@ const fetch = require('node-fetch');
 
 const TNM_API_BASE_URL = 'https://tnmaccess.nationalmap.gov';
 
-export async function getProducts(boundingBox: string) {
+// TODO: adjust method to accept bounding box instead of polygon
+export async function getProducts(polygon: string) {
   const searchUrl = new URL('/api/v1/products', TNM_API_BASE_URL);
-  searchUrl.searchParams.append('prodFormats', 'GeoTIFF&datasets=Digital Elevation Model (DEM) 1 meter,National Elevation Dataset (NED) 1/3 arc-second,National Elevation Dataset (NED) 1/9 arc-second');
-  searchUrl.searchParams.append('polygon', boundingBox);
+  searchUrl.searchParams.append('prodFormats', 'GeoTIFF');
+  searchUrl.searchParams.append('datasets', 'Digital Elevation Model (DEM) 1 meter,National Elevation Dataset (NED) 1/3 arc-second,National Elevation Dataset (NED) 1/9 arc-second');
   searchUrl.searchParams.append('outputFormat', 'JSON');
 
-  return fetch(searchUrl);
+  // API does not expect polygon param's commas to be URL encoded (which occurs when using searchParams)
+  const finalSearchUrl = `${searchUrl.href}&polygon=${polygon}`;
+
+  const response = await fetch(finalSearchUrl);
+
+  if (response.ok) {
+    return response.json();
+  }
+  return '[]'
 }
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,10 @@
 const { getProducts } = require('../src/api');
 
 describe("TNM API", () => {
-  it("should return 17 products", async () => {
+  it("should return 13 products", async () => {
     const boundingBox = '-122.674011196934%2038.0220728663461,-122.52459715119774%2038.0220728663461,-122.52459715119774%2037.93374140015236,-122.674011196934%2037.93374140015236,%20-122.674011196934%2038.0220728663461';
     const response = await getProducts(boundingBox);
-    expect(response).toBeDefined();
+    expect(response.errors.length).toBe(0);
+    expect(response.items.length).toBe(13);
   });
 });
